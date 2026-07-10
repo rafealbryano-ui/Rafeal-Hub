8cf43dcdd6f78573bb1109212d318afd731acabc
Rafeal <rafealbryano@gmail.com>
2026-07-10 12:54:02 -0700

    Update M.lua

--- Assets/APIs/M.lua
+++ Assets/APIs/M.lua
@@ -11935,16 +11935,12 @@ local function SetupTeleportRejoin()
     local gameFile = FreeLoad[GameId] and FreeLoad[GameId].File or (KeyLoad[GameId] and KeyLoad[GameId].File);
     if not gameFile then gameFile = "7597195391"; end
     local scriptSource = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/rafealbryano-ui/Rafeal-Hub/refs/heads/main/ListFile/' .. gameFile .. '.lua"))()';
-    local TeleportService = game:GetService("TeleportService");
-    if TeleportService then
-        local connection
-        connection = game:GetService("RunService").Heartbeat:Connect(function()
-            if TeleportService.TeleportState == Enum.TeleportState.Teleporting then
-                queue_on_teleport(scriptSource);
-                if connection then connection:Disconnect() end
-            end
-        end)
-    end
+    game:GetService("RunService").Heartbeat:Connect(function()
+        if game.PlaceId ~= PlaceId then
+            queue_on_teleport(scriptSource);
+            game:GetService("RunService").Heartbeat:Disconnect();
+        end
+    end)
 end
 
 SetupTeleportRejoin();
