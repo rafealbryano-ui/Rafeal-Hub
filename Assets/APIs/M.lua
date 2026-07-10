c450d70f57e39499b32447b5079b9cc74dad36a4
Rafeal <rafealbryano@gmail.com>
2026-07-10 13:36:29 -0700

    Remove queue_on_teleport logic from M.lua
    Removed teleport queue logic and related variables.

--- Assets/APIs/M.lua
+++ Assets/APIs/M.lua
@@ -11930,20 +11930,6 @@ local FreeLoad, KeyLoad = {
     };
 };
 
-if queue_on_teleport then
-    local gameFile = FreeLoad[GameId] and FreeLoad[GameId].File or (KeyLoad[GameId] and KeyLoad[GameId].File);
-    if not gameFile then gameFile = "7597195391"; end
-    local scriptSource = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/rafealbryano-ui/Rafeal-Hub/refs/heads/main/ListFile/' .. gameFile .. '.lua"))()';
-    local currentJobId = game.JobId;
-    game:GetService("RunService").Heartbeat:Connect(function()
-        if game.JobId ~= currentJobId then
-            queue_on_teleport(scriptSource);
-            currentJobId = game.JobId;
-        end
-    end)
-end
-
-local Resolve = FreeLoad[GameId] and FreeLoad[GameId].File .. ".lua";
 
 GG.LoadFromVControl = LoadFromVControl;
 GG.LoaderSettings = LoaderSettings;
